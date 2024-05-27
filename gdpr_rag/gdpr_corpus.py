53120e7a182da73f29e7353989359d172fa6cb3a
steven davey <daveysj@gmail.com>
2024-05-27 09:43:37 +0200

    covid health

--- gdpr_rag/gdpr_corpus.py
+++ gdpr_rag/gdpr_corpus.py
@@ -13,6 +13,7 @@ from gdpr_rag.documents.codes import Codes
 from gdpr_rag.documents.online_services import OnlineServices
 from gdpr_rag.documents.territorial_scope import TerritorialScope
 from gdpr_rag.documents.video import Video
+from gdpr_rag.documents.covid_health import CovidHealth
 
 
 
