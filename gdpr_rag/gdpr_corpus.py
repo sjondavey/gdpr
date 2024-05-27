6a3fc031f6d2384a5ac0487fff58a3582eb5e47f
steven davey <daveysj@gmail.com>
2024-05-27 13:15:24 +0200

    Covid location

--- gdpr_rag/gdpr_corpus.py
+++ gdpr_rag/gdpr_corpus.py
@@ -14,6 +14,7 @@ from gdpr_rag.documents.online_services import OnlineServices
 from gdpr_rag.documents.territorial_scope import TerritorialScope
 from gdpr_rag.documents.video import Video
 from gdpr_rag.documents.covid_health import CovidHealth
+from gdpr_rag.documents.covid_location import CovidLocation
 
 
 
