f9f321e80ecb5d40ff2341dc96ff10321770cf17
steven davey <daveysj@gmail.com>
2024-05-30 11:23:10 +0200

    dpia md

--- gdpr_rag/documents/dpia.py
+++ gdpr_rag/documents/dpia.py
@@ -3,23 +3,7 @@ import pandas as pd
 from regulations_rag.regulation_reader import  load_csv_data
 from regulations_rag.document import Document
 from regulations_rag.reference_checker import ReferenceChecker
-
 from regulations_rag.reference_checker import MultiReferenceChecker
-
-def extract_footnotes(text):
-    lines = text.split('\n')
-    footnotes = []
-    remaining_text = []
-
-    for line in lines:
-        if re.match(r'^\[\^\d+\]', line):
-            footnotes.append(line)
-        else:
-            remaining_text.append(line)
-
-    text = '\n'.join(remaining_text)
-    return footnotes, text
-
 
 class DPIA(Document):
     def __init__(self, path_to_manual_as_csv_file = "./inputs/documents/dpia.parquet"):
@@ -47,109 +31,12 @@ class DPIA(Document):
                 return False
         return True
 
-    def _add_numbering_to_text(self, row, text_extract, heading = False):
-        if row["heading"]:
-            if row["point"] != "":
-                return row["point"] + ". " + text_extract
-            elif row["subsection"] != "":
-                return row["subsection"] + ". " + text_extract
-            else:
-                return row["section"] + ". " + text_extract
-        else:    
-            if heading:
-                return ""
-            if row["point"]:
-                return " " * 8 + text_extract
-            elif row["subsection"]:
-                return " " * 4 +  text_extract
-            else:
-                return text_extract
-
-    def get_text(self, section_reference):               
-        if not (section_reference == "" or self.reference_checker.is_valid(section_reference)):
-            return "" 
-        else:
-            footnote_pattern = r'\[\^\d+\]'
-
-            text = ""
-            all_footnotes = []
-            if section_reference == "":
-                subset = self.document_as_df
-            else:
-                subset = self.document_as_df[self.document_as_df["section_reference"] == section_reference] 
-
-            if len(subset) == 0:
-                return ""
-            for index, row in subset.iterrows():
-                if row['text'].startswith("|"): # not table 
-                    pass
-                elif row['text'].strip() == "The following examples illustrate how the criteria should be used to assess whether a particular processing operation requires a DPIA:":
-                    pass
-                else: # process  
-                    footnotes, text_extract = extract_footnotes(row["text"])
-                    all_footnotes = all_footnotes + footnotes
-                    text += self._add_numbering_to_text(row, text_extract) + "\n"
-            parent = self.reference_checker.get_parent_reference(section_reference)
-            build_up = ""
-            while parent != "":
-
-                subset = self.document_as_df[self.document_as_df["section_reference"] == parent]
-                for index, row in subset[::-1].iterrows(): # backwards
-                    if row["heading"]:
-                        footnotes, text_extract = extract_footnotes(row["text"])
-                        all_footnotes = all_footnotes + footnotes
-                        build_up = self._add_numbering_to_text(row, text_extract) + "\n" + build_up
+    def get_text(self, section_reference, add_markdown_decorators = True, footnote_pattern = r'^\[\^\d+\]\:'):               
+        return super().get_text_for_section_only(section_reference, add_markdown_decorators, footnote_pattern)
 
 
-                parent = self.reference_checker.get_parent_reference(parent)
-            if build_up != "":
-                text = build_up + "\n" + text
-            for footnote in all_footnotes:
-                text = text + "\n" + footnote
-
-            return text.strip()
-
-
-    def get_heading(self, section_reference):
-        if not self.reference_checker.is_valid(section_reference):
-            return "" 
-        else:
-            footnote_pattern = r'\[\^\d+\]'
-
-            text = ""
-            all_footnotes = []
-            subset = self.document_as_df[self.document_as_df["section_reference"] == (section_reference)]
-            if len(subset) > 0:
-                for index, row in subset.iterrows():
-                    footnotes, text_extract = extract_footnotes(row["text"])
-                    formatted_text = self._add_numbering_to_text(row, text_extract, True)
-                    if formatted_text:
-                        all_footnotes = all_footnotes + footnotes
-                        if text == "":
-                            text = formatted_text
-                        else:
-                            text += "\n" + formatted_text
-
-                parent = self.reference_checker.get_parent_reference(section_reference)
-                build_up = ""
-                while parent != "":
-                    subset = self.document_as_df[self.document_as_df["section_reference"] == parent]
-                    for index, row in subset[::-1].iterrows(): # backwards
-                        footnotes, text_extract = extract_footnotes(row["text"])
-                        formatted_text = self._add_numbering_to_text(row, text_extract, True)
-                        if formatted_text:
-                            all_footnotes = all_footnotes + footnotes
-                            if build_up == "":
-                                build_up = formatted_text    
-                            else:
-                                build_up = formatted_text + "\n" + build_up
-
-                    parent = self.reference_checker.get_parent_reference(parent)
-                text = build_up + "\n" + text
-                for footnote in all_footnotes:
-                    text = text + "\n" + footnote
-
-            return text.strip()
+    def get_heading(self, section_reference, add_markdown_decorators = False, footnote_pattern = r'^\[\^\d+\]\:'):
+        return super().get_heading(section_reference, add_markdown_decorators, footnote_pattern)
 
 
     class DecisionMakingReferenceChecker(ReferenceChecker):
